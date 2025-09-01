8130aba71b83e6d91d38edfe54155d54b8468837
suman jaiswal <jsonusuman351@gmail.com>
2025-09-01 23:20:58 +0530

    modify document similarity

--- document_similarity.py
+++ document_similarity.py
@@ -26,7 +26,7 @@ query_embedding = embedding.embed_query(query)
 
 scores = cosine_similarity([query_embedding], doc_embeddings)[0]
 
-index, score = sorted(list(enumerate(scores)),key=lambda )[-1]
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
 
 print(query)
 print(documents[index])
